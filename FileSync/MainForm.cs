416314ed74ba96945c765ceb86877406e78a18b4
pengjianaixue <pengjianaixue@hotmail.com>
2021-01-18 17:50:18 +0800

    adjust the order of show messagebox

--- FileSync/MainForm.cs
+++ FileSync/MainForm.cs
@@ -500,10 +500,10 @@ namespace FileSync
             }
             catch (System.Exception ex)
             {
-                MessageBox.Show(string.Format("Sftp Client occur exception: {0},Please check config !", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Show();
                 Activate();
                 SwtichToWindows.swtichToWindows(this);
+                MessageBox.Show(string.Format("Sftp Client occur exception: {0},Please check config !", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
         }
@@ -572,12 +572,13 @@ namespace FileSync
             }
             catch (System.Exception ex)
             {
-                MessageBox.Show(string.Format("File: {0} Download Faild! Error info: {1}",
-                    FileChangeGridView.Rows[e.RowIndex].Cells[0].Value.ToString(), ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
                 updateUploadStatusDisplay(e.RowIndex, "Download", fullFilePath, 1);
                 Show();
                 Activate();
                 SwtichToWindows.swtichToWindows(this);
+                MessageBox.Show(string.Format("File: {0} Download Faild! Error info: {1}",
+                    FileChangeGridView.Rows[e.RowIndex].Cells[0].Value.ToString(), ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             isInDownload = false;
 
